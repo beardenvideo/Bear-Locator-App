d5f0768b9ea271d11aea5ae1576557ef7496b40e
Maggie Carpenter <carpmh17@mit.edu>
2019-02-20 20:31:23 -0500

    Initial Commit

---
+++ Bear Locator App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bear Locator App
+//
+//  Created by Margaret Carpenter on 2/20/19.
+//  Copyright © 2019 Margaret Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Bear Locator AppTests/Bear_Locator_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bear_Locator_AppTests.swift
+//  Bear Locator AppTests
+//
+//  Created by Margaret Carpenter on 2/20/19.
+//  Copyright © 2019 Margaret Carpenter. All rights reserved.
+//
+
+import XCTest
+@testable import Bear_Locator_App
+
+class Bear_Locator_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
